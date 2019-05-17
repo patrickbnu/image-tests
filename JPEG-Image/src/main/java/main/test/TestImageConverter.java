9a70d5be3f521e91cf1fee4f63ebcd17bf7e9493
patricK <terceiro.dbr@posthaus.com.br>
2019-05-17 17:13:21 -0300

    images

--- JPEG-Image/src/main/java/main/test/TestImageConverter.java
+++ JPEG-Image/src/main/java/main/test/TestImageConverter.java
@@ -26,13 +26,36 @@ public class TestImageConverter {
 	private static final float compression = 0.6f;
 	
 	public static void main(String[] args) throws IOException, ImageProcessingException {
-		BufferedImage image = ImageIO.read(new File("image/original.jpg"));
+		// transformImage(ImageIO.read(new File("image/original.jpg")), "image/api/original.jpg");
+		
+		File folder = new File("images/originals");
+		File[] listOfFiles = folder.listFiles();
+		for (File image : listOfFiles) {
+			transformImage(ImageIO.read(image), "images/api/" + image.getName());
+			compareImageSize(image.getName());
+		}
+	}
+	
+	private static void compareImageSize(String name) {
+		File original = new File("images/originals/" + name);
+		File photoshop = new File("images/photoshop/" + name);
+		File api = new File("images/api/" + name);
+		
+		System.out.println(original.length() + " original");
+		System.out.println(photoshop.length() + " photoshop");
+		System.out.println(api.length() + " api");
+		
+		System.out.println();
+		
+	}
+	
+	private static void transformImage(BufferedImage image, String fileName) throws IOException {
 		
 		image = JPEGImageHelper.resizeByWidth(image, width);
 		
 		image = JPEGImageHelper.adjustHeight(image, height);
 		
-		JPEGImageHelper.saveImage(new File("image/best.jpg"), image, dpi, compression);
+		JPEGImageHelper.saveImage(new File(fileName), image, dpi, compression);
 	}
 	
 	// https://stackoverflow.com/questions/19224328/how-to-read-jpeg-file-attributes-with-java
